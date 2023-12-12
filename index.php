93b667b8c46330aec5f9cc98043bdd3afd04dd4f
Victor Invernon Garcia <145662511+1vaitor10@users.noreply.github.com>
2023-12-12 09:13:37 +0100

    mostrar fet

--- index.php
+++ index.php
@@ -17,39 +17,53 @@
         <a class="navbar-brand" href="#">Inicio</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
-        </button>
+  </head>
+<body>
+<!--content start-->
+</button>
         <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav">
-               
-                <a class="nav-link active" aria-current="page" href="index.php?Controller=producte&action=mostrartot">productes</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Insertar</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Editar</a>
-                </li>
-            </ul>
-        </div>
-        <div class="ml-auto">
-    <form class="form-inline" id="registerForm" action="registre.php" method="post" enctype="multipart/form-data">
+          <a class="nav-link active" aria-current="page" href="index.php?Controller=producte&action=mostrartot">Producte</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="index.php?controller=nota&action=llistar">Notes</a>
+        </li>
+    </div>
+    <div class="ml-auto">
+    <form class="form-inline" id="registerForm" action="register.php" method="post" enctype="multipart/form-data">
         <a class="btn btn-light mr-2" href="login.php">Login</a>
         <button type="submit" class="btn btn-primary">Registrar</button>
     </form>
 </div>
-            
-        </div>
-    </nav>
+  </div>
+</nav>
 
-    <section class="container mt-5">
-        <h2>Contenido Principal</h2>
-        <p>Este es un ejemplo de una página web con Bootstrap. Puedes utilizar las clases de Bootstrap para dar estilo a tus elementos y hacer que la página sea receptiva.</p>
-    </section>
 
-    <footer class="bg-dark text-white text-center py-3">
-        <p>&copy; <?php echo date("Y"); ?> Mi Página con Bootstrap</p>
-    </footer>
 
+
+<?php 
+
+require_once "autoload.php";
+
+if(isset($_GET["Controller"]) && class_exists($_GET["Controller"]) ){
+    $nomcontroller = $_GET["Controller"] . "Controller";
+    $controller = new $nomcontroller();
+
+    if(isset($_GET["action"]) && method_exists($controller,$_GET["action"])){
+        $action = $_GET["action"];
+        $controller->$action();
+    }
+    else{
+        echo $_GET["action"] . " no existeix aquest metode";
+    }
+}
+else{
+    
+    echo $_GET["controller"] .  " no existeix el controlador";
+}
+
+
+?>
     <!-- Modal de Registro -->
     <div class="modal fade" id="registerModal" tabindex="-1" role="dialog" aria-labelledby="registerModalLabel" aria-hidden="true">
         <div class="modal-dialog" role="document">
@@ -95,7 +109,9 @@
             </div>
         </div>
     </div>
-
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+<!--content end-->
+<!-- JavaScript -->
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.6.0/dist/umd/popper.min.js" integrity="sha384-KsvD1yqQ1/1+IA7gi3P0tyJcT3vR+NdBTt13hSJ2lnve8agRGXTTyNaBYmCR/Nwi" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.min.js" integrity="sha384-nsg8ua9HAw1y0W1btsyWgBklPnCUAFLuTMS2G72MMONqmOymq585AcH49TLBQObG" crossorigin="anonymous"></script>
+</body>
+</html>

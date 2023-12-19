ca736348a56101dbb51eb6fee387b7b0fa1160e4
Samuel0327 <145454201+Samuel0327@users.noreply.github.com>
2023-12-19 01:30:48 +0100

    login i registre

--- login.php
+++ login.php
@@ -1,62 +1,76 @@
 <?php
-    session_start();
-    if (isset($_POST['submit'])) {
-        $nom = $_POST['nom'];
-        $contrasenya = $_POST['contrasenya'];
-        $server = "localhost";
-        $user = "root";
-        $password = "";
-        $dbname = "gestio_de_stock";
+session_start();
 
-        // Conectar
-        $db = new mysqli($server, $user, $password, $dbname);
-        // Comprobar conexión
-        if ($db->connect_error) {
-            die("La conexión ha fallado, error número " . $db->connect_errno . ": " . $db->connect_error);
-        }
+if (isset($_POST['submit'])) {
+    $nom = $_POST['nom'];
+    $contrasenya = $_POST['contrasenya'];
+    $server = "localhost";
+    $user = "root";
+    $password = "";
+    $dbname = "gestio_de_stock";
 
-        $stmt = $db->prepare("SELECT `contraseña`,`nombre_usuario` FROM `usuarios` WHERE `nombre_usuario`=? ");
-        $stmt->bind_param('s', $nom);
-        $stmt->execute();
-        $result = $stmt->get_result();
-        $row = $result->fetch_assoc();
-        $stmt->close();
-        $db->close();
-
-        if (password_verify($contrasenya, $row['contraseña'])) {
-            $_SESSION['nom'] = $nom;
-            header("Location: mostrar.php");
-            exit(); // Asegura que no se envíe nada más después de la redirección
-        } else {
-            echo "Les credencials son incorrectes";
-        }
+    // Conectar
+    $db = new mysqli($server, $user, $password, $dbname);
+    // Comprobar conexión
+    if ($db->connect_error) {
+        die("La conexión ha fallado, error número " . $db->connect_errno . ": " . $db->connect_error);
     }
 
-    if (isset($_GET["logout"]) && $_GET["logout"] == 1) {
-        session_destroy();
-        header("Location: login.php");
+    $stmt = $db->prepare("SELECT `contraseña`,`nombre_usuario` FROM `usuarios` WHERE `nombre_usuario`=? ");
+    $stmt->bind_param('s', $nom);
+    $stmt->execute();
+    $result = $stmt->get_result();
+    $row = $result->fetch_assoc();
+    $stmt->close();
+    $db->close();
+
+    if (password_verify($contrasenya, $row['contraseña'])) {
+        $_SESSION['nom'] = $nom;
+        header("Location: mostrar.php");
         exit(); // Asegura que no se envíe nada más después de la redirección
+    } else {
+        echo "Les credencials son incorrectes";
     }
+}
+
+if (isset($_GET["logout"]) && $_GET["logout"] == 1) {
+    session_destroy();
+    header("Location: login.php");
+    exit(); // Asegura que no se envíe nada más después de la redirección
+}
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Login</title>
+    <!-- Enlaces CDN de Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
-<body>
-    <form action="login.php" method="post">
-        <label for="nom">Nom</label><br>
-        <input type="text" name="nom" id="" required placeholder="Introdueix el teu nom">
-        <br>
-        <label for="Contrasenya">Contrasenya</label><br>
-        <input type="password" name="contrasenya" id="" required placeholder="Introdueix la teva contrasenya">
-        <br>
-        <input type="submit" name="submit" value="Entrar">
-    </form>
 
-    <a href="register.php"><button>Registrat</button></a>
+<body>
+    <div class="container mt-5">
+        <form action="login.php" method="post">
+            <div class="mb-3">
+                <label for="nom" class="form-label">Nom</label>
+                <input type="text" name="nom" class="form-control" required placeholder="Introdueix el teu nom">
+            </div>
+            <div class="mb-3">
+                <label for="contrasenya" class="form-label">Contrasenya</label>
+                <input type="password" name="contrasenya" class="form-control" required placeholder="Introdueix la teva contrasenya">
+            </div>
+            <button type="submit" name="submit" class="btn btn-primary">Entrar</button>
+        </form>
+
+        <a href="register.php" class="mt-3 btn btn-secondary">Registrat</a>
+    </div>
+
+    <!-- Enlaces CDN de Bootstrap JS y Popper.js (requerido para ciertos componentes de Bootstrap) -->
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js"></script>
 </body>
+
 </html>
--- register.php
+++ register.php
@@ -1,62 +1,73 @@
+<?php
+if (isset($_POST['submit'])) {
+    $nom = $_POST['nom'];
+    $contrasenya = $_POST['contrasenya'];
+    $contrasenya2 = $_POST['contrasenya2'];
+    
+    if ($contrasenya == $contrasenya2) {
+        $server = "localhost";
+        $user = "root";
+        $password = "";
+        $dbname = "gestio_de_stock";
+
+        // Conectar
+        $db = new mysqli($server, $user, $password, $dbname);
+
+        // Comprobar conexión
+        if ($db->connect_error) {
+            die("La conexión ha fallado, error número " . $db->connect_errno . ": " . $db->connect_error);
+        } else {
+            $contrasenya = password_hash($contrasenya, PASSWORD_DEFAULT);
+            $stmt = $db->prepare("INSERT INTO `usuari` (`username`, `Contrasenya`) VALUES (?, ?)");
+            $stmt->bind_param('ss', $nom, $contrasenya);
+            $stmt->execute();
+            $stmt->close();
+            $db->close();
+
+            header("Location:login.php"); // Redirigir con PHP
+            exit(); // Asegura que no se envíe nada más después de la redirección
+        }
+    } else {
+        echo "Registro fallido: Las contraseñas no coinciden";
+    }
+}
+?>
+
+<!DOCTYPE html>
 <html lang="en">
 
 <head>
-
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Registro</title>
+    <!-- Enlaces CDN de Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
+
 <body>
-    <form action="register.php" method="post">
-        <label for="nom">Nom</label><br>
-        <input type="text" name="nom" id="" required placeholder="Introdueix el teu nom">
-        <br>
-        <br>
-        <label for="contrasenya">Contrasenya</label><br>
-        <input type="password" name="contrasenya" id="" required placeholder="Introdueix la teva contrasenya">
-        <br>
-        <br>
-        <label for="contrasenya">Torna a introduir la contrasenya</label><br>
-        <input type="password" name="contrasenya2" id="" required placeholder="Repeteix la teva contrasenya">
-        <br>
-        <br>
-        <input type="submit" name="submit" value="Registrarse">
-    </form>
-        <?php
-            if(isset($_POST['submit'])){
-                $nom = $_POST['nom'];
-                $contrasenya = $_POST['contrasenya'];
-                $contrasenya2 = $_POST['contrasenya2'];
-                if($contrasenya == $contrasenya2){
-                    echo "111";
-                    $server = "localhost";
-                    $user = "root";
-                    $password = "";
-                    $dbname = "gestoi_de_stock";
-                    // Conectar
-                    $db = new mysqli($server, $user, $password, $dbname);
-                    echo "222";
-                    // Comprobar conexión
-                    if($db->connect_error){
-                        die("La conexión ha fallado, error número " . $db->connect_errno . ": " . $db->connect_error);
-                    }
-                    else{
-                    $contrasenya=password_hash($contrasenya, PASSWORD_DEFAULT);
-                    $stmt = $db->prepare("INSERT INTO `usuari` (`username`, `Contrasenya`) VALUES (?, ?)");
-                    $stmt->bind_param('ss', $nom, $contrasenya);
-                    $stmt->execute();
-                    $stmt->close();
-                    $db->close();
+    <div class="container mt-5">
+        <form action="register.php" method="post">
+            <div class="mb-3">
+                <label for="nom" class="form-label">Nom</label>
+                <input type="text" name="nom" class="form-control" required placeholder="Introdueix el teu nom">
+            </div>
+            <div class="mb-3">
+                <label for="contrasenya" class="form-label">Contrasenya</label>
+                <input type="password" name="contrasenya" class="form-control" required placeholder="Introdueix la teva contrasenya">
+            </div>
+            <div class="mb-3">
+                <label for="contrasenya2" class="form-label">Torna a introduir la contrasenya</label>
+                <input type="password" name="contrasenya2" class="form-control" required placeholder="Repeteix la teva contrasenya">
+            </div>
+            <button type="submit" name="submit" class="btn btn-primary">Registrarse</button>
+        </form>
 
-                    header("Location:login.php"); //Redirigir amb php
-                    }
-                }else{
-                    echo "Registre Failed";
-                }
+        <a href="login.php" class="mt-3 btn btn-secondary">Ya tengo cuenta</a>
+    </div>
 
-            }
-        ?>
-    <a href="login.php"><button>Ya tengo cuenta</button></a>
-
+    <!-- Enlaces CDN de Bootstrap JS y Popper.js (requerido para ciertos componentes de Bootstrap) -->
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js"></script>
 </body>
+
 </html>
